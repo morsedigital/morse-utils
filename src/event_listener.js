86649b1a07ae0c50f163e09ecc0f8657905af79e
Adrian Stainforth <adrian.stainforth@gmail.com>
2018-04-30 15:39:30 +0100

    Upgrade

--- src/event_listener.js
+++ src/event_listener.js
@@ -1,23 +1,20 @@
-import {
-  isArray
-  , isElement
-} from 'lodash';
+import { isArray, isElement } from "lodash";
 
-const checkParent = (check, elm)=>{
+const checkParent = (check, elm) => {
   if (!isElement(elm.parentNode)) return null;
   if (check(elm.parentNode)) return elm.parentNode;
   if (elm.parentNode === document.body) return null;
-  return checkParent(check, elm.parentNode)
-}
+  return checkParent(check, elm.parentNode);
+};
 
-const manageEvents = (modules)=>{
-  return function(e){
+const manageEvents = modules => {
+  return function(e) {
     let element = e.target;
 
-    modules.forEach((mod)=>{
-      let {check, trigger} = mod;
-
-      if (check(element)){
+    modules.forEach(mod => {
+      let { check, trigger } = mod;
+      console.log(check(element), element);
+      if (check(element)) {
         trigger(element, e);
       } else {
         const elm = checkParent(check, element);
@@ -25,15 +22,15 @@ const manageEvents = (modules)=>{
       }
     });
   };
-}
+};
 
-export default function(modules){
-  if (!isArray) throw new Error('Must be array');
+export default function(modules) {
+  if (!isArray) throw new Error("Must be array");
 
   var eventHandler = manageEvents(modules);
-  document.body.addEventListener('click', eventHandler, false);
+  document.body.addEventListener("click", eventHandler, false);
 
-  return function(){
-    document.body.removeEventListener('click', eventHandler, false);
+  return function() {
+    document.body.removeEventListener("click", eventHandler, false);
   };
-};
+}
